2355a0a49812f85f9c069082f16248329fa1a9ee
areino <areino@users.noreply.github.com>
2022-10-24 19:51:37 +0200

    Ported to falconpy SDK
    Cloud URL and token caching config no longer needed

--- creds.py
+++ creds.py
@@ -9,24 +9,9 @@ proxy_hostname = "proxyhost.domain.com"
 proxy_port = "8080"
 
 
-## Select your cloud
-
-cloud_domain = "https://api.crowdstrike.com"             # US1 cloud
-# cloud_domain = "https://api.us-2.crowdstrike.com"         # US2 cloud
-# cloud_domain = "https://api.eu-1.crowdstrike.com"         # EU Cloud
-# cloud_domain = "https://api.laggar.gcw.crowdstrike.com"   # Gov Cloud
-
-## Enter CID
-
-cid = "A0C63F1116634DC6A3658027D05D9718"
-
 ## Enter your API credentials
 
 api_client_id           = "ENTER YOUR API CLIENT DETAILS HERE"
 api_client_secret       = "ENTER YOUR API CLIENT DETAILS HERE"
 
 
-## Access bearer token caching
-
-max_duration = 1799 ## seconds
-token_filename = "token.txt"
