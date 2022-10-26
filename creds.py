d6142839ada2d18fe1ab3b2b53080c779226f755
areino <areino@users.noreply.github.com>
2022-10-26 15:34:27 +0200

    Update creds.py

--- creds.py
+++ creds.py
@@ -8,6 +8,10 @@
 proxy_hostname = "proxyhost.domain.com"
 proxy_port = "8080"
 
+## Enter CID or Host Group ID (host groups are recommended for testing purposes)
+
+scope = "hostgroup" ## Values can be "cid" or "hostgroup"
+scope_id = "ENTER HOST GROUP ID OR CID HERE" ## ID of hostgroup or CID
 
 ## Enter your API credentials
 
